144b3435c7cdcb4e96616a2c3cc579bacf02952e
s105061522 <b19930917@gmail.com>
2025-07-26 17:41:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,41 @@
+// server.js
+const express = require('express');
+const axios = require('axios');
+const crypto = require('crypto');
+const app = express();
+app.use(express.json());
+
+const CHANNEL_ID = process.env.CHANNEL_ID;
+const CHANNEL_SECRET = process.env.CHANNEL_SECRET;
+const BASE_URL = 'https://sandbox-api-pay.line.me';
+
+function makeSignature(uri, body, nonce) {
+  const text = CHANNEL_SECRET + uri + body + nonce;
+  return crypto.createHmac('sha256', CHANNEL_SECRET).update(text).digest('base64');
+}
+
+app.post('/pay', async (req, res) => {
+  const body = req.body;
+  const uri = '/v3/payments/request';
+  const nonce = Date.now().toString();
+  const jsonBody = JSON.stringify(body);
+  const signature = makeSignature(uri, jsonBody, nonce);
+
+  try {
+    const response = await axios.post(BASE_URL + uri, jsonBody, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-LINE-ChannelId': CHANNEL_ID,
+        'X-LINE-Authorization-Nonce': nonce,
+        'X-LINE-Authorization': signature
+      }
+    });
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).json({ error: err.toString(), details: err.response?.data });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on ${port}`));
 
